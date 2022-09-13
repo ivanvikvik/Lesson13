418bb791f7c9c60c8b370b82839158e63cd3e743
vikvik <ivanvikvik@gmail.com>
2022-09-13 11:40:02 +0300

    Using code without class static methods

---
+++ src/Task07.java
@@ -0,0 +1,13 @@
+public class Task07 {
+
+    public static void sum() {
+        // logic
+        int result = 5 + 6;
+        System.out.println(result);
+    }
+
+
+    public static void main(String[] args) {
+        //Task07.sum();
+    }
+}
