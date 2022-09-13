ac1db4182779fcc2b8215a49c805b068cc522d31
vikvik <ivanvikvik@gmail.com>
2022-09-13 11:58:18 +0300

    Using code of Task07 with class static methods

--- src/Task07.java
+++ src/Task07.java
@@ -1,13 +1,22 @@
 public class Task07 {
 
-    public static void sum() {
+    public static int sum(int a, int b) {
         // logic
-        int result = 5 + 6;
+        int result = a + b;
+        return result;
+    }
+
+    public static void print(int result) {
+        // output
         System.out.println(result);
     }
 
 
     public static void main(String[] args) {
-        //Task07.sum();
+//        int result = sum(16,7);
+//        print(result);
+        System.out.println(sum(0, 0) == 0);
+        System.out.println(sum(1, 2) == 3);
+        System.out.println(sum(3, 4) == 8);
     }
 }
--- src/Task08.java
+++ src/Task08.java
@@ -1,8 +1,5 @@
 public class Task08 {
     public static void main(String[] args) {
-        Task07 task07 = new Task07();
-        task07.sum();
 
-        Task07.sum();
     }
 }
